d01c7853da869a0b4824da0ab757c3baba21a872
Christoffer Petersen <fantomlich.fujie@gmail.com>
2020-05-06 11:43:50 +0200

    Leg med liste

--- Homepage/Hjemmeside/index.aspx.cs
+++ Homepage/Hjemmeside/index.aspx.cs
@@ -10,15 +10,34 @@ namespace Hjemmeside
 {
     public partial class index : System.Web.UI.Page
     {
+        public static List<Person> people = new List<Person>();
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
         [WebMethod]
         public static bool ProcessData(string name)
         {
             Console.WriteLine(name);
+            people.Add(new Person(people.Count, name));
             return name != null;
+        }
+    }
+
+    public class Person
+    {
+        int id;
+        string name;
+        double latitude;
+        double longitude;
+
+        public Person(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+            //this.latitude = latitude;
+            //this.longitude = longitude;
         }
     }
 }
